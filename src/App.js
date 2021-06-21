176e22c9ca14ae48ca37ff7b4a796c5795797e0f
ranjith210297 <ranjeeth2197@msitprogram.net>
2021-06-21 09:55:20 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Aboutus from "./components/Aboutus";
+import BootstrapNavbar from "./components/BootstrapNavbar";
+import Contactus from "./components/Contactus";
+//import Home from "./components/Home";
+import Predictor from "./components/Predictor";
+import ScrollBar from "react-perfect-scrollbar";
+import LoginButton from "./components/LoginButton";
+import Login from "./components/login";
+import "react-perfect-scrollbar/dist/css/styles.css";
+// import "./example.scss";
+
+
+
+function App() {
+  return (
+    <div>
+    <ScrollBar component="div">
+    <Router>
+      <BootstrapNavbar />
+      
+      <LoginButton />
+      
+      <Route exact path="/Login">
+        <Login />
+      </Route>
+    {/* <Login /> */}
+      <Route exact path="/">
+        <Predictor />
+      </Route>
+
+      <Route path="/Contactus" component={Contactus} />
+      <Route path="/Aboutus" component={Aboutus} />
+      <Route path="/Predictor" component={Predictor} />
+    
+    {/* < Footer /> */}
+    </Router>
+    </ScrollBar>
+    </div>
+  );
+}
+
+export default App;
